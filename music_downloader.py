412ed8041bf5993cdcd9dc7c1598af59285f7ace
Tim Hanson <tim.hanson@appfolio.com>
2017-06-16 12:44:53 -0700

    changed file

--- music_downloader.py
+++ music_downloader.py
@@ -1,1 +1,13 @@
-import os
+###################################################################################
+###     Author:         Tim Hanson                                              ###
+###     Date:           06/16/2017                                              ###
+###     Title:          Music Downloader                                        ###
+###     Description:    Uses web crawler and web tools to download and convert  ###
+###                 soundcloud music to mp3 files and tags them accordingly.    ###
+###                 Also adds cover art after downloading from cover art        ###
+###                 download site for each song.                                ###
+###################################################################################
+
+
+import scrapy
+

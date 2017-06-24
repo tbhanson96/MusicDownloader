d33d2b95b4e9eb9f9c3f102c358cc0f4452c9b9f
Tim Hanson <tim.hanson@appfolio.com>
2017-06-23 21:03:08 -0700

    added album art download and mutagen library

--- music_downloader.py
+++ music_downloader.py
@@ -2,7 +2,7 @@
 ###     Author:         Tim Hanson                                              ###
 ###     Date:           06/16/2017                                              ###
 ###     Title:          Music Downloader                                        ###
-###     Description:    Uses selenium and pytaglib  to download and convert     ###
+###     Description:    Uses selenium and mutagen to download and convert       ###
 ###                 soundcloud music to mp3 files and tags them accordingly.    ###
 ###                 Also adds cover art after downloading from cover art        ###
 ###                 download site for each song.                                ###
@@ -14,19 +14,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import taglib
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, TIT2, TPE1, TALB
 import json
-import sys, os
+import sys, os, shutil, glob
 from json_maker import JsonMaker
 
 CONVERTER_URL = 'http://clipconverter.cc'
-FORM_NAME = 'converter'
-URL_ELEMENT = 'mediaurl'
-SUBMIT_ELEMENT = 'submit'
+COVER_DOWNLOAD_URL = 'http://soundcloudcoverdownloader.pe.hu'
 
 def download_music(media_url, browser):
     browser.get(CONVERTER_URL)
-    elem = browser.find_element_by_name(URL_ELEMENT)
+    elem = browser.find_element_by_name('mediaurl')
     elem.send_keys(media_url)
     elem.send_keys(Keys.RETURN)
 
@@ -39,11 +38,32 @@ def download_music(media_url, browser):
 
     wait = WebDriverWait(browser, 10)
     download = wait.until(EC.visibility_of_element_located((By.ID, 'downloadbutton')))
+
     download.click()
 
     new = browser.find_element_by_link_text('Convert another video')
     new.click()
 
+    while glob.glob('*.crdownload'):
+        pass
+
+def download_cover_art(media_url, browser):
+    browser.get(COVER_DOWNLOAD_URL)
+    url_input = browser.find_element_by_id('query_input')
+    url_input.send_keys(media_url)
+
+    submit_button = browser.find_element_by_css_selector('button.sccdgreen')
+    submit_button.click()
+
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.visibility_of_element_located((By.ID, 'psongname')))
+    download = browser.find_element_by_css_selector('img.responsive-img')
+    download.click()
+
+    while glob.glob('*.crdownload'):
+        pass
+
+    
 def setup_browser():
     options = webdriver.ChromeOptions()
     cur_path = os.getcwd();
@@ -52,21 +72,46 @@ def setup_browser():
     chromedriver = 'chromedriver'
     return webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
 
-def parse_json_and_tag_files(json_file):
-    json_string = open(json_file).read()
-    json_object = json.loads(json_string)
-    for song in json_object:
-        song_data = json_object[song]
-        
+def tag_media(media_file_name, cover_file_name, tag_dict):
+    media_file = MP3(media_file_name, ID3=ID3)
+    if not media_file.tags:
+        media_file.add_tags()
+    media_file.tags.add(TIT2(encoding=3, text=tag_dict['TITLE']))
+    media_file.tags.add(TPE1(encoding=3, text=tag_dict['ARTIST']))
+    media_file.tags.add(TALB(encoding=3, text=tag_dict['TITLE']))
+    
+    media_file.tags.add(APIC(
+                    encoding=3,
+                    mime='image/'+cover_file_name.split('.')[1],
+                    desc='Cover',
+                    data=open(cover_file_name).read()
+                    )
+    )
 
+    media_file.save()
+    print media_file.tags
+    new_file_name = tag_dict['ARTIST'] + ' - ' + tag_dict['TITLE'] + '.mp3'
+    shutil.move(media_file_name, new_file_name)
 
 
 def main():
-    download_music('https://soundcloud.com/lucianofficial/lucian-do-my-thing-ft-philosofie', setup_browser())
+    tag_file = open('music.json')
+    json_tags = json.load(tag_file)
+    chrome = setup_browser()
+
+    os.chdir('media')
+    for entry in json_tags:
+        media_url = entry
+        media_tags = json_tags[entry]
+        download_music(media_url, chrome)
+        media_file_name = max([f for f in os.listdir('.')], key=os.path.getctime)
+        download_cover_art(media_url, chrome)
+        cover_file_name = max([f for f in glob.glob('*')], key=os.path.getctime)
+        chrome.close()
+        tag_media(media_file_name, cover_file_name, media_tags)
+    
 
 if __name__ == '__main__':
     main()
 
-        
-   
 

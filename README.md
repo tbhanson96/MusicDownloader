88a2239c753b2184885cbfba96cea6116e4609d5
tbhanson96 <tbh20715@gmail.com>
2017-06-24 12:45:53 -0700

    checking for downloaded files more stable, download dir is now media/download

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### Author:
 Tim Hanson                                                                                                                                
 ### Description:
-Uses selenium and pytaglib  to download and convert     
+Uses selenium and mutagen to download and convert     
 soundcloud music to mp3 files and tags them accordingly.    
 Also adds cover art after downloading from cover art        
 download site for each song.                                
--- music_downloader.py
+++ music_downloader.py
@@ -17,11 +17,12 @@ from selenium.webdriver.common.keys import Keys
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, APIC, TIT2, TPE1, TALB
 import json
-import sys, os, shutil, glob
+import sys, os, shutil, glob, time
 from json_maker import JsonMaker
 
 CONVERTER_URL = 'http://clipconverter.cc'
 COVER_DOWNLOAD_URL = 'http://soundcloudcoverdownloader.pe.hu'
+TAG_FILE = 'to_add.json'
 
 def download_music(media_url, browser):
     browser.get(CONVERTER_URL)
@@ -30,22 +31,20 @@ def download_music(media_url, browser):
     elem.send_keys(Keys.RETURN)
 
 
-    wait = WebDriverWait(browser, 10)
+    wait = WebDriverWait(browser, 15)
     submit = wait.until(EC.visibility_of_element_located((By.ID, 'submitconvert')))
 
     button = submit.find_element_by_css_selector('input')
     button.click()
 
-    wait = WebDriverWait(browser, 10)
+    wait = WebDriverWait(browser, 15)
     download = wait.until(EC.visibility_of_element_located((By.ID, 'downloadbutton')))
 
     download.click()
 
-    new = browser.find_element_by_link_text('Convert another video')
-    new.click()
+    #new = browser.find_element_by_link_text('Convert another video')
+    #new.click()
 
-    while glob.glob('*.crdownload'):
-        pass
 
 def download_cover_art(media_url, browser):
     browser.get(COVER_DOWNLOAD_URL)
@@ -60,14 +59,11 @@ def download_cover_art(media_url, browser):
     download = browser.find_element_by_css_selector('img.responsive-img')
     download.click()
 
-    while glob.glob('*.crdownload'):
-        pass
-
-    
+ 
 def setup_browser():
     options = webdriver.ChromeOptions()
     cur_path = os.getcwd();
-    prefs = {'download.default_directory' : '{}/media'.format(cur_path)}
+    prefs = {'download.default_directory' : cur_path + '/media/download'}
     options.add_experimental_option('prefs',prefs)
     chromedriver = 'chromedriver'
     return webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
@@ -76,39 +72,78 @@ def tag_media(media_file_name, cover_file_name, tag_dict):
     media_file = MP3(media_file_name, ID3=ID3)
     if not media_file.tags:
         media_file.add_tags()
-    media_file.tags.add(TIT2(encoding=3, text=tag_dict['TITLE']))
-    media_file.tags.add(TPE1(encoding=3, text=tag_dict['ARTIST']))
-    media_file.tags.add(TALB(encoding=3, text=tag_dict['TITLE']))
+    media_file.tags.add(TIT2(encoding=3, text=tag_dict['title']))
+    media_file.tags.add(TPE1(encoding=3, text=tag_dict['artist']))
+    media_file.tags.add(TALB(encoding=3, text=tag_dict['title']))
     
     media_file.tags.add(APIC(
                     encoding=3,
-                    mime='image/'+cover_file_name.split('.')[1],
+                    mime='image/'+cover_file_name.split('.')[-1],
                     desc='Cover',
                     data=open(cover_file_name).read()
                     )
     )
 
     media_file.save()
-    print media_file.tags
-    new_file_name = tag_dict['ARTIST'] + ' - ' + tag_dict['TITLE'] + '.mp3'
-    shutil.move(media_file_name, new_file_name)
 
+def loading_bar(current, total):
+    new_bar = '\r['
+    for i in xrange(current):
+        new_bar += '=' 
+    new_bar += '>'
+    for i in xrange(total - current):
+        new_bar += '_'  
+    new_bar += ']'
+    return new_bar
+
+
+def find_and_move_file(path, extension, new_files_name):
+    files_to_search = path + '/*.' + extension
+    return_files = []
+    while not return_files:
+        return_files = glob.glob(files_to_search)
+
+    return_file_name = return_files[0]
+    os.rename(return_file_name, '{}.{}'.format(new_files_name, extension))
+    return '{}.{}'.format(new_files_name, extension)
+    
 
 def main():
-    tag_file = open('music.json')
+    print 'Initializing Selenium and starting download...'
+
+    tag_file = open(TAG_FILE)
     json_tags = json.load(tag_file)
     chrome = setup_browser()
+    log_file = open('log.txt', 'w')
 
+    if not os.path.exists('media'):
+        os.mkdir('media')
     os.chdir('media')
+
+    num_finished = 0
+    print loading_bar(num_finished, len(json_tags.keys())),
     for entry in json_tags:
         media_url = entry
         media_tags = json_tags[entry]
+        new_files_name = '{} - {}'.format(media_tags['artist'], media_tags['title'])
+
+        if os.path.exists(new_files_name+'.mp3'):
+            log_file.write('skipped {}\n'.format(new_files_name))
+            num_finished += 1
+            print loading_bar(num_finished, len(json_tags.keys())),
+            continue
+
         download_music(media_url, chrome)
-        media_file_name = max([f for f in os.listdir('.')], key=os.path.getctime)
+        media_file_name = find_and_move_file('download', 'mp3', new_files_name)
         download_cover_art(media_url, chrome)
-        cover_file_name = max([f for f in glob.glob('*')], key=os.path.getctime)
-        chrome.close()
+        cover_file_name = find_and_move_file('download', 'jpg', new_files_name)
         tag_media(media_file_name, cover_file_name, media_tags)
+
+        log_file.write('completed: {}\n'.format(new_files_name))        
+        num_finished += 1
+        print loading_bar(num_finished, len(json_tags.keys())),
+
+    chrome.close()
     
 
 if __name__ == '__main__':
